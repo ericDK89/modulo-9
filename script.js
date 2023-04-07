90f40ecf405d506668c2f24e2e4ad541f2e3403d
ericDK89 <ericthr42@gmail.com>
2023-04-07 17:16:51 -0300

    add: jquery script to add new img done

--- script.js
+++ script.js
@@ -9,5 +9,25 @@ $(document).ready(() => {
 
   $("form").on("submit", (e) => {
     e.preventDefault();
+
+    const imgUrl = $("#add-img-input").val();
+
+    const newLine = $("<li></li>");
+
+    $(`
+    <img src="${imgUrl}" />
+    <div class="link-div">
+      <a
+        href="${imgUrl}"
+        title="Ver imagem em tamanho real"
+        target="_blank"
+        >Ver imagem em tamanho real</a
+      >
+    </div>
+    `).appendTo(newLine);
+
+    $(newLine).appendTo("ul");
+
+    $("#add-img-input").val("");
   });
 });
